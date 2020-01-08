3edbad901574eeccc34a52071ce7903499bd1232
Bastian Ilso <hougaard.junior@gmail.com>
2020-01-08 15:21:36 +0100

    Update Ports for SynchExercise
    Updated the Ports to fit with the new shield design.

--- Synch/Synch.ino
+++ Synch/Synch.ino
@@ -1,8 +1,8 @@
-int inputButton = 2;
-int userLED = 11;
-int userVib = 3;
-int testLED = 10;
-int testVib = 5;
+int inputButton = 12;
+int userLED = 10;
+int userVib = 2;
+int testLED = 11;
+int testVib = 1;
 
 //  ************** STUDENTS CHANGE VARIABLES BELOW
 
